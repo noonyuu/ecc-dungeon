3e4767d00d912ead49601eff310cda8a9ee30106
yuki <2240022@ecc.ac.jp>
2024-12-02 21:42:35 +0900

    change ボタンのスタイル変更

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import { VariantProps, cva } from 'class-variance-authority';
 
-export const buttonVariants = cva(
-  `bg-gradient-to-r from-[#543F2C] to-[#2A2416] h-20`,
-  {
-    variants: {
-      size: {
-        small: `w-52`,
-        medium: `w-[304px]`,
-      },
+export const buttonVariants = cva(``, {
+  variants: {
+    size: {
+      mini: `w-32`,
+      small: `w-52`,
+      medium: `w-[304px]`,
+      large: `w-44`,
     },
-    defaultVariants: {
-      size: 'small',
+    visual: {
+      normal: ``,
+      outline: `h-20 bg-gradient-to-r from-[#543F2C] to-[#2A2416]`,
     },
   },
-);
+  defaultVariants: {
+    size: 'small',
+    visual: 'outline',
+  },
+});
 
 export const innerVariants = cva(
-  `bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-[#63832D] to-[#485922] text-white text-2xl font-semibold border-solid border-4 border-[#526428] mx-auto h-16 flex items-center justify-center`,
+  `bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-[#63832D] to-[#485922] text-white font-semibold border-solid border-4 border-[#526428] mx-auto h-16 flex items-center justify-center`,
   {
     variants: {
       size: {
-        small: `w-48`,
-        medium: `w-72`,
+        mini: `w-28 text-xl`,
+        small: `w-48 text-2xl`,
+        medium: `w-72 text-2xl`,
+        large: `w-40 text-3xl`,
       },
     },
     defaultVariants: {
@@ -31,18 +37,26 @@ export const innerVariants = cva(
   },
 );
 
-interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  onClick: () => void; // ボタンがクリックされたときの処理
-  label: string; // ボタンに表示するテキスト
-}
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+  size?: 'mini' | 'small' | 'medium' | 'large';
+  visual?: 'normal' | 'outline';
+};
 
 // ボタンコンポーネント
-const Button: React.FC<ButtonProps> = ({ onClick, label, size = 'small' }) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  size = 'small',
+  visual = 'outline',
+  className,
+  ...props
+}) => {
   return (
-    <button className={`${buttonVariants({ size })}`} onClick={onClick}>
-      <div className={`${innerVariants({ size })}`}>{label}</div>
+    <button
+      className={`${buttonVariants({ size, visual })} ${className} `}
+      {...props}
+    >
+      <div className={`${innerVariants({ size })}`}>{children}</div>
     </button>
   );
 };
